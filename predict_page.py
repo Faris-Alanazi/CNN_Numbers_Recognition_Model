a50981d32a1d30bd1c8075ed557c80d1c37cd013
fares-alanazi <ifares.alanazi@gmail.com>
2022-12-23 03:27:04 +0300

    Fixed Page Layout

--- predict_page.py
+++ predict_page.py
@@ -2,11 +2,8 @@ import streamlit as st
 from keras.models import load_model
 import pandas as pd
 import cv2 
-import tensorflow 
-import pickle
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
-
 
 predictions = []
 classes = [
@@ -27,29 +24,35 @@ def load():
     return load_model('model.h5')
 cnn_model = load()
 
-def show_page():
-    st.title('Number Recognition Model')
-    st.subheader("Draw Any Number Between 0 and 9 , I Bet You I'll Predict it") 
 
-    canvas_result = st_canvas(
-        fill_color='pink',
-        height=500,
-        width=700,
-        stroke_width=25,
-        stroke_color='white',
+def show_page():
+    col1, col2, col3 = st.columns([1,3,1])
+
+    with col1:
+        pass
+    with col2:
+        st.markdown("<h2 style='text-align: center; color: #83c9ff;'>Number Recognition Model</h2>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center; color: #83c9ff;'>Draw Any Number Between 0 and 9</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center; color: #83c9ff;'>I Bet You I'll Predict it</h4>", unsafe_allow_html=True)
+        st.write("")
+        canvas_result = st_canvas(
+        height=400,
+        width=385,
+        stroke_width=22,
+        stroke_color='#83c9ff',
         background_color='black',
         update_streamlit=True,
         drawing_mode="freedraw",
         key="canvas",
-    )
-    
-    pred = st.button("Predicit",key='btn')
+        )
+        pred = st.button("Predicit")
+        if pred:
+            predicit(canvas_result.image_data,col2,col3)
+    with col3 :
+        pass
+         
 
-    if pred:
-        predicit(canvas_result.image_data)
-
-
-def predicit(canvas):
+def predicit(canvas,col2,col3):
     img  = np.array(canvas) 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (28,28))
@@ -62,12 +65,12 @@ def predicit(canvas):
     for r in results:
         predictions.append([classes[r[0]],float("{:.2f}".format(r[1]*100))])
     preds = pd.DataFrame([x[1] for x in predictions],[x[0] for x in predictions])
-    col1, col2 = st.columns([3, 1])
-    col1.subheader("The Predicitons Probilites in %")
-    col1.bar_chart(data=preds, use_container_width=True)
-    col2.subheader("Model Predicitons is")
-    col2.header(np.argmax(pred))
-   
+    
+    with col2:
+        st.write("")
+        st.subheader("The Predicitons is : " + str(np.argmax(pred)))
+        st.markdown("<h4 style='text-align: center; color: #83c9ff;'>The Predicitons Probilites in %</h4>", unsafe_allow_html=True)
+        st.bar_chart(data=preds, use_container_width=True)
     
    
     
